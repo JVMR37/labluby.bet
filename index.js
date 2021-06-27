d8995e9ee97a8496f856c5e64606dad2663b2324
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-06-27 10:02:55 -0400

    Carregamento do JSON, ações dos botões e seleção de números

--- index.js
+++ index.js
@@ -9,58 +9,146 @@
         var $quinaGameButton = $('[data-js="quinaButton"]').get();
 
 
+        var $gameDescriptionText = $('[data-js="gameDescriptionText"]').get();
+
+
+        var selectedGame = {
+            "type": "",
+            "description": "",
+            "range": 0,
+            "price": 0,
+            "max-number": 0,
+            "color": "",
+            "min-cart-value": 0
+        };
+
+        var gameTypeList = [];
+
+        var selectedNumbersElementList = [];
+
+        var cart = [
+            {
+                "selectedNumber": [0, 2050, 1, 5],
+                "typeGame": "LotoLoto",
+                "price": 151.00
+            }
+        ]
+
+
         function initialize() {
-            $typeGameText.textContent = "Lotofácil";
-            $lotofacilGameButton.classList.add('lotofacilButtonSelectedStyle');
+            getGameTypes();
+            changeTypeGame('Lotofácil');
             initEvents();
         }
 
         function initEvents() {
-            $('[data-js="lotofacilButton"]').on('click', changeTypeGame);
-            $('[data-js="megaSenaButton"]').on('click', changeTypeGame);
-            $('[data-js="quinaButton"]').on('click', changeTypeGame);
+            $('[data-js="lotofacilButton"]').on('click', handleGameRadioButtonClick);
+            $('[data-js="megaSenaButton"]').on('click', handleGameRadioButtonClick);
+            $('[data-js="quinaButton"]').on('click', handleGameRadioButtonClick);
+            $('[data-js="clearGameButton"]').on('click', handleClearGameButton);
+            $('[data-js="completeGameButton"]').on('click', handleCompleteGameButton);
+            // ? Apagar o elementos selecionados ao clicar no botão de completar ou
+            // ? Terminar de preencher
         }
 
-        function changeTypeGame(e) {
+        function getGameTypes() {
+            var ajax = new XMLHttpRequest();
+            ajax.open('GET', './assets/games.json', false);
+
+            ajax.send();
+
+            var gamesJsonDecoded = JSON.parse(ajax.responseText);
+            gameTypeList = gamesJsonDecoded.types;
+
+            console.log(gameTypeList);
+        }
+
+        function handleGameRadioButtonClick(e) {
             e.preventDefault();
             console.log(e);
-            var button = e.target;
-            var typeGameButton = button.getAttribute('data-js');
-            console.log(button);
+            var typeGameButton = e.target.getAttribute('data-js');
+            var newTypeGame;
 
             switch (typeGameButton) {
                 case 'lotofacilButton':
-                    $typeGameText.textContent = "Lotofácil";
+                    newTypeGame = "Lotofácil";
+                    break;
+                case 'megaSenaButton':
+                    newTypeGame = "Mega-Sena";
+                    break;
+                case 'quinaButton':
+                    newTypeGame = "Quina";
+                    break;
+            }
+
+            changeTypeGame(newTypeGame);
+
+        }
+
+        function handleClearGameButton() {
+            removeAllSelectedElements();
+        }
+
+        function handleCompleteGameButton(e) {
+            randomlySelectNumbers();
+        }
+
+        function removeAllSelectedElements() {
+            selectedNumbersElementList.forEach(
+                function (numberElement) {
+                    numberElement.classList.remove('lotofacilBackgroundColor');
+                    numberElement.classList.remove('megaSenaBackgroundColor');
+                    numberElement.classList.remove('quinaBackgroundColor');
+                }
+            );
+
+            console.log(selectedNumbersElementList);
+
+            selectedNumbersElementList = [];
+
+        }
+
+        function changeTypeGame(newTypeGame) {
+            selectedGame = gameTypeList.filter(
+                function (gameObject) {
+                    return gameObject.type === newTypeGame
+                }
+            )[0];
+
+            $gameDescriptionText.textContent = selectedGame.description;
+            $typeGameText.textContent = selectedGame.type;
+
+            selectedNumbersElementList = [];
+            buildGridWithNumbers(selectedGame.range);
+
+
+            switch (selectedGame.type) {
+                case 'Lotofácil':
                     $lotofacilGameButton.classList.add('lotofacilButtonSelectedStyle');
                     $megaSenaGameButton.classList.remove('megaSenaButtonSelectedStyle');
                     $quinaGameButton.classList.remove('quinaButtonSelectedStyle');
 
-                    buildGridWithNumbers(25);
                     break;
-
-                case 'megaSenaButton':
-                    $typeGameText.textContent = "Mega-Sena";
+                case 'Mega-Sena':
 
                     $lotofacilGameButton.classList.remove('lotofacilButtonSelectedStyle');
                     $megaSenaGameButton.classList.add('megaSenaButtonSelectedStyle');
                     $quinaGameButton.classList.remove('quinaButtonSelectedStyle');
 
-
-                    buildGridWithNumbers(60)
-
                     break;
-
-                case 'quinaButton':
-                    $typeGameText.textContent = "Quina";
+                case 'Quina':
 
                     $lotofacilGameButton.classList.remove('lotofacilButtonSelectedStyle');
                     $megaSenaGameButton.classList.remove('megaSenaButtonSelectedStyle');
                     $quinaGameButton.classList.add('quinaButtonSelectedStyle');
 
-                    buildGridWithNumbers(80);
-
                     break;
             }
+        }
+
+        function hasSelectedNumber(numberElement) {
+            //TODO: Implemente isso aqui mano, e usa na funçaõ de selecionar e na ativação do botão
+            //TOPDO: No botão ta pra chmar uma função especifica só de remover
         }
 
         function buildGridWithNumbers(numbersQuantity) {
@@ -69,16 +157,90 @@
 
 
             for (let index = 1; index <= numbersQuantity; index++) {
-                var divNumber = doc.createElement('div');
+                var buttonNumber = doc.createElement('button');
 
-                divNumber.classList.add('col-1', 'numberCircleStyle', 'd-flex', 'justify-content-center');
-                divNumber.textContent = ('0' + index).slice(-2);
+                buttonNumber.classList.add('col-1', 'btn', 'numberCircleStyle', 'd-flex', 'justify-content-center');
+                buttonNumber.textContent = ('0' + index).slice(-2);
 
-                $fragment.appendChild(divNumber);
+                buttonNumber.addEventListener('click', handleClickNumberButton);
+
+                $fragment.appendChild(buttonNumber);
             }
 
             numberOfGameArea.innerHTML = '';
             numberOfGameArea.appendChild($fragment);
+        }
+
+        function handleClickNumberButton(e) {
+            e.preventDefault();
+            var numberElement = e.target;
+
+            if (selectedNumbersElementList.length === selectedGame["max-number"]) {
+                win.alert("Você já selecionou o máximo de números permitidos para esse jogo !");
+                return;
+            }
+
+            selectNumber(numberElement, true);
+        }
+
+        function selectNumber(numberElement, removeRepeated) {
+            switch (selectedGame.type) {
+                case 'Lotofácil':
+
+                    if (numberElement.classList.contains('lotofacilBackgroundColor')) {
+                        if (!removeRepeated) return
+                        numberElement.classList.remove('lotofacilBackgroundColor');
+                        selectedNumbersElementList.remove(numberElement);
+                        return;
+                    }
+
+                    selectedNumbersElementList.push(numberElement);
+
+                    numberElement.classList.add('lotofacilBackgroundColor');
+                    break;
+                case 'Mega-Sena':
+
+                    if (numberElement.classList.contains('megaSenaBackgroundColor')) {
+                        if (!removeRepeated) return
+
+                        numberElement.classList.remove('megaSenaBackgroundColor');
+                        var indexToRemove = selectedNumbersElementList.indexOf(numberElement);
+                        selectedNumbersElementList.splice(indexToRemove, 1);
+
+                        return;
+                    }
+
+                    selectedNumbersElementList.push(numberElement);
+                    numberElement.classList.add('megaSenaBackgroundColor');
+
+
+                    break;
+                case 'Quina':
+                    if (numberElement.classList.contains('quinaBackgroundColor')) {
+                        if (!removeRepeated) return
+
+                        numberElement.classList.remove('quinaBackgroundColor');
+                        selectedNumbersElementList.remove(numberElement);
+                        console.log(selectedNumbersElementList.length);
+
+                        return;
+                    }
+
+                    selectedNumbersElementList.push(numberElement);
+                    numberElement.classList.add('quinaBackgroundColor');
+
+                    break;
+            }
+        }
+
+        function randomlySelectNumbers() {
+            const numberOfGameArea = doc.querySelector('[data-js="numberOfGameArea"]');
+            while (selectedNumbersElementList.length < selectedGame["max-number"]) {
+                var index = Math.ceil(Math.random() * (selectedGame.range - 1));
+                var number = numberOfGameArea.childNodes[index];
+                selectNumber(number, false);
+            }
+
         }
 
         initialize();

c66b6164fc02ff684827109e5dca7a9aeee1eb70
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-26 19:28:03 -0400

    feat: Add Reset Password Button

--- labluby.bet/src/components/LoginCard.tsx
+++ labluby.bet/src/components/LoginCard.tsx
@@ -4,18 +4,21 @@ import { FaArrowRight } from "react-icons/fa";
 
 import React from "react";
 import styled from "styled-components";
+import { ResetPasswordButton } from "../styles/loginCard.style";
+import { useHistory } from "react-router-dom";
 
-interface IProps extends React.HTMLProps<HTMLInputElement> {}
+// interface IProps extends React.HTMLProps<HTMLInputElement> {}
 
 const LoginCard: React.FC = () => {
-  const aloha: IProps = {
-    placeholder: "email",
-  };
-
   const Form = styled.form`
     padding-top: 10px;
-    /* padding-bottom: 10px; */
   `;
+
+  const history = useHistory();
+
+  const handleResetPassButton = () => {
+    history.push("/reset-password");
+  };
 
   return (
     <Card>
@@ -27,7 +30,13 @@ const LoginCard: React.FC = () => {
 
         <hr />
 
-        <FlatButton isPrimary>Log In <FaArrowRight/></FlatButton>
+        <ResetPasswordButton onClick={handleResetPassButton}>
+          I forget my password
+        </ResetPasswordButton>
+
+        <FlatButton isPrimary>
+          Log In <FaArrowRight />
+        </FlatButton>
       </Form>
     </Card>
   );
--- labluby.bet/src/styles/loginCard.style.ts
+++ labluby.bet/src/styles/loginCard.style.ts
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
-export const LoginCard = styled.div`
-    
+export const ResetPasswordButton = styled.button`
+  text-align: left;
+
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  margin-right: 1rem;
+
+  float: right;
+
+  font: italic 17px Helvetica;
+  letter-spacing: 0px;
+  color: #c1c1c1;
+  opacity: 1;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
 `;

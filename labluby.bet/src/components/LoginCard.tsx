1da7f3395aae09a852b8c6472f78a990f41d3059
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-27 18:07:22 -0400

    fix: Tirando a declaração do styled component de dentrod da FC

--- labluby.bet/src/components/LoginCard.tsx
+++ labluby.bet/src/components/LoginCard.tsx
@@ -1,32 +1,116 @@
 import Card from "../layout/Card";
+import { InputField } from "../layout/Input";
+
 import { FlatButton } from "../GlobalStyles";
 import { FaArrowRight } from "react-icons/fa";
 
 import React from "react";
 import styled from "styled-components";
 import { ResetPasswordButton } from "../styles/loginCard.style";
+import ErrotInputTextStyled from "../styles/errorInputText.style";
 import { useHistory } from "react-router-dom";
+import useInput from "../hooks/use-input";
+import { emailValidator, passValidator } from "../utils/validators";
 
 // interface IProps extends React.HTMLProps<HTMLInputElement> {}
+const Form = styled.form`
+  padding-top: 10px;
+`;
 
 const LoginCard: React.FC = () => {
-  const Form = styled.form`
-    padding-top: 10px;
-  `;
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(emailValidator);
 
+  const {
+    value: passValue,
+    isValid: passIsValid,
+    hasError: passHasError,
+    valueChangeHandler: passChangeHandler,
+    inputBlurHandler: passBlurHandler,
+    reset: resetPass,
+  } = useInput(passValidator);
   const history = useHistory();
 
   const handleResetPassButton = () => {
     history.push("/reset-password");
   };
 
+  let formIsValid = false;
+
+  if (passIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const submitHandler = (event: any) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    console.log("Submitted!");
+
+    resetEmail();
+    resetPass();
+  };
+
+  const EmailInput = (
+    <InputField
+      type="text"
+      value={emailValue}
+      onChange={emailChangeHandler}
+      onBlur={emailBlurHandler}
+      id="1"
+      key={"email"}
+      // autoFocus={true}
+      placeholder="Email"
+    ></InputField>
+  );
+
   return (
     <Card>
-      <Form>
-        <input placeholder="Email"></input>
+      <Form onSubmit={submitHandler}>
+        <div>
+          <InputField
+            type="text"
+            value={emailValue}
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+            id="1"
+            key="email"
+            placeholder="Email"
+          ></InputField>
+
+          {emailHasError && (
+            <ErrotInputTextStyled>
+              Please enter a valid email address.
+            </ErrotInputTextStyled>
+          )}
+        </div>
 
         <hr />
-        <input placeholder="Password"></input>
+        <input
+          type="password"
+          value={passValue}
+          onChange={passChangeHandler}
+          onBlur={passBlurHandler}
+          id="2"
+          key={"pass"}
+          // autoFocus={true}
+          placeholder="Password"
+        ></input>
+
+        {passHasError && (
+          <ErrotInputTextStyled>
+            Please enter a valid password.
+          </ErrotInputTextStyled>
+        )}
 
         <hr />
 
@@ -34,7 +118,7 @@ const LoginCard: React.FC = () => {
           I forget my password
         </ResetPasswordButton>
 
-        <FlatButton isPrimary>
+        <FlatButton isPrimary disabled={!formIsValid}>
           Log In <FaArrowRight />
         </FlatButton>
       </Form>
--- labluby.bet/src/layout/Card.tsx
+++ labluby.bet/src/layout/Card.tsx
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 
+const Div = styled.div`
+  background: ${({ theme }) => theme.colors.card} 0% 0% no-repeat padding-box;
+  box-shadow: 0px 3px 25px #00000014;
+  border: 1px solid #dddddd;
+  border-radius: 14px;
+  opacity: 1;
+  padding: 0px;
+
+  position: relative;
+
+  display: inline-block;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Card: React.FC = (props) => {
-  const Div = styled.div`
-    background: ${({ theme }) => theme.colors.card} 0% 0% no-repeat padding-box;
-    box-shadow: 0px 3px 25px #00000014;
-    border: 1px solid #dddddd;
-    border-radius: 14px;
-    opacity: 1;
-    padding: 0px;
-
-    position: relative;
-
-    display: inline-block;
-    justify-content: center;
-    align-items: center;
-  `;
-
   return <Div>{props.children}</Div>;
 };
 

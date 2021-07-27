d938664c9ac869ff9b4c2d66c3e074834da9d065
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-26 20:34:49 -0400

    feat: Controle de acesso baseado na autenticação

--- labluby.bet/src/App.tsx
+++ labluby.bet/src/App.tsx
@@ -2,21 +2,42 @@ import Layout from "./layout/Layout";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import { Switch, Route } from "react-router-dom";
+import ResetPass from "./pages/ResetPass";
+import Home from "./pages/Home";
+import NewBet from "./pages/NewBet";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useAppSelector } from "./hooks/hooks";
+import { selectIsLoggedInValue } from "./store/authSlice";
 
 function App() {
+  const isLoggedIn = useAppSelector(selectIsLoggedInValue);
+
   return (
     <Layout>
       <Switch>
-        <Route path="/login">
-          <Login></Login>
+        <Route path="/" exact>
+          {isLoggedIn && <Redirect to="/home"></Redirect>}
+          {!isLoggedIn && <Redirect to="/login"></Redirect>}
         </Route>
-        <Route path="/register">
+
+        <Route path="/home">
+          {isLoggedIn && <Home />}
+          {!isLoggedIn && <Redirect to="/login"></Redirect>}
+        </Route>
+
+        <Route path="/new-bet">
+          {isLoggedIn && <NewBet />}
+          {!isLoggedIn && <Redirect to="/login"></Redirect>}
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register" exact>
           <Register />
         </Route>
-        <Route path="/reset-password"></Route>
-        <Route path="/order-now">
-          <span>aloha</span>
+        <Route path="/reset-password">
+          <ResetPass />
         </Route>
 
         <Route path="*">

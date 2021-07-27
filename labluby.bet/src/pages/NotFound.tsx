d0eeb810a46e3d98490136ecdc2be6dad1cdcf03
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-26 20:37:43 -0400

    feat: Finished NotFound Page

--- labluby.bet/src/pages/NotFound.tsx
+++ labluby.bet/src/pages/NotFound.tsx
@@ -1,9 +1,30 @@
-import { CenteredDiv } from "../GlobalStyles";
+import { CenteredDiv, FlatButton, Column } from "../GlobalStyles";
+import { FaArrowLeft } from "react-icons/fa";
+import { useHistory } from "react-router-dom";
+import { NotFoundSpan } from "../styles/notFound.style";
+import { useAppSelector } from "../hooks/hooks";
+import { selectIsLoggedInValue } from "../store/authSlice";
 
 const NotFound = () => {
+  const history = useHistory();
+  const isLoggedIn = useAppSelector(selectIsLoggedInValue);
+
+  const handleBackButton = () => {
+    history.replace(isLoggedIn ? "/home" : "/login");
+  };
+
   return (
     <CenteredDiv>
-      <p>Page not found!</p>
+      <Column>
+        <NotFoundSpan>
+          Oops...
+          <br /> Page not found.
+        </NotFoundSpan>
+        <FlatButton isPrimary onClick={handleBackButton}>
+          <FaArrowLeft />
+          Return to {isLoggedIn ? "Home" : "Login"}
+        </FlatButton>
+      </Column>
     </CenteredDiv>
   );
 };

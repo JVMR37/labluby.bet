7c03f835c7f43edf7dce1ff905e5689ec69a5c4d
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-26 20:38:10 -0400

    feat: add page files without implementation

---
+++ labluby.bet/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Home: React.FC = () => {
+  //TODO: Implement Home Page
+
+  return <div></div>;
+};
+
+export default Home;
---
+++ labluby.bet/src/pages/NewBet.tsx
@@ -0,0 +1,6 @@
+const NewBet: React.FC = () => {
+  //TODO: Implement NewBet Page
+    return <div></div>;
+};
+
+export default NewBet;

6f8b019c7ba2387b771a5d65a57f8ac569a59877
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-28 17:25:58 -0400

    feat: implementation of Login Thunk Function

--- labluby.bet/src/store/authSlice.ts
+++ labluby.bet/src/store/authSlice.ts
@@ -1,38 +1,82 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from ".";
 import User from "../models/User";
+import { loginInAPI } from "../datasource/authDataSource";
 
 export interface AuthState {
-  user: User | null;
+  userId: string | null;
+  userToken: string | null;
+  userName: string | null;
+  userEmail: string | null;
+
   isLoggedIn: boolean;
+  isLoading: boolean;
 }
 
 const initialState: AuthState = {
-  user: null,
+  userId: null,
+  userToken: null,
+  userName: null,
+  userEmail: null,
+
   isLoggedIn: false,
+  isLoading: false,
 };
+
+export interface LoginProps {
+  email: string;
+  password: string;
+}
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async (props: LoginProps) => {
+    const response = await loginInAPI(props.email, props.password);
+    // The value we return becomes the `fulfilled` action payload
+    return { ...response.data };
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login: (state, action: PayloadAction<User>) => {
-      state.user = action.payload;
-      state.isLoggedIn = true;
-    },
-
     logout: (state) => {
-      state.user = null;
-      state.isLoggedIn = false;
+      Object.assign(state, initialState);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state, action) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.isLoading = false;
+      const user = action.payload;
+
+      if (user) {
+        state.userId = user.id;
+        state.userToken = user.id;
+        state.userName = user.name;
+        state.userEmail = user.email;
+
+        state.isLoggedIn = true;
+      }
+    });
   },
 });
 
 export const selectIsLoggedInValue = (state: RootState) =>
   state.auth.isLoggedIn;
 
-export const selectUser = (state: RootState) => state.auth.user;
+export const selectUserData = (state: RootState) => {
+  return {
+    id: state.auth.userId,
+    email: state.auth.userEmail,
+    name: state.auth.userName,
+  };
+};
 
-export const { login, logout } = authSlice.actions;
+export const { logout } = authSlice.actions;
 
 export default authSlice.reducer;

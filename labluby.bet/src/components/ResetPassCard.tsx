255654c4e2ccd3ec56f4d4ef25510cbedeb17881
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-28 11:06:27 -0400

    feat: reset pass form implementation

--- labluby.bet/src/components/ResetPassCard.tsx
+++ labluby.bet/src/components/ResetPassCard.tsx
@@ -4,11 +4,51 @@ import Card from "../layout/Card";
 import { FlatButton, StyledForm } from "../GlobalStyles";
 import { FaArrowRight } from "react-icons/fa";
 
+import useInput from "../hooks/use-input";
+import { emailValidator } from "../utils/validators";
+import { InputField } from "../layout/Input";
+import ErrotInputTextStyled from "../styles/errorInputText.style";
+
 const ResetPassCard: React.FC = () => {
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(emailValidator);
+
+  const submitHandler = (event: any) => {
+    event.preventDefault();
+
+    if (!emailIsValid) {
+      return;
+    }
+
+    console.log("Submitted!");
+
+    resetEmail();
+  };
+
   return (
     <Card>
-      <StyledForm>
-        <input placeholder="Email"></input>
+      <StyledForm onSubmit={submitHandler}>
+        <InputField
+          type="text"
+          value={emailValue}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          id="1"
+          key="email"
+          placeholder="Email"
+        ></InputField>
+
+        {emailHasError && (
+          <ErrotInputTextStyled>
+            Please enter a valid email address.
+          </ErrotInputTextStyled>
+        )}
         <hr />
 
         <FlatButton isPrimary>

8bb557d89646cd3bccdfa1ba9103ac2eff62d39f
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-28 11:09:29 -0400

    1 pixel increase in border radius

--- labluby.bet/src/styles/card.style.ts
+++ labluby.bet/src/styles/card.style.ts
@@ -4,7 +4,7 @@ export const StyledCard = styled.div`
   background: ${({ theme }) => theme.colors.card} 0% 0% no-repeat padding-box;
   box-shadow: 0px 3px 25px #00000014;
   border: 1px solid #dddddd;
-  border-radius: 14px;
+  border-radius: 15px;
   opacity: 1;
   padding: 0px;
 

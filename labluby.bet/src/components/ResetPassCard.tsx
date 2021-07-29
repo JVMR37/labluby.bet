badd59edc2da0f23ed6a6db2983425b782014dfb
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-29 17:16:33 -0400

    feat: Connect ResetPassCard to AuthSlice and feedback to user

--- labluby.bet/src/components/ResetPassCard.tsx
+++ labluby.bet/src/components/ResetPassCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Card from "../layout/Card";
 import { FlatButton, StyledForm } from "../GlobalStyles";
@@ -10,6 +10,17 @@ import { InputField } from "../layout/Input";
 import ErrotInputTextStyled from "../styles/errorInputText.style";
 import AnimatedDivStyled from "../styles/animatedDiv.style";
 
+import { useHistory } from "react-router-dom";
+
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import AuthStatusMessage from "./AuthStatusMessageComponent";
+import {
+  sendLinkToResetPass,
+  updateAuthStatusAfterTime,
+  AuthStatus,
+  selectAuthStatusValue,
+} from "../store/authSlice";
+
 const ResetPassCard: React.FC = () => {
   const {
     value: emailValue,
@@ -20,11 +31,56 @@ const ResetPassCard: React.FC = () => {
     reset: resetEmail,
   } = useInput(emailValidator);
 
-  const submitHandler = (event: any) => {
+  const history = useHistory();
+  const dispatch = useAppDispatch();
+  const authStatus = useAppSelector(selectAuthStatusValue);
+
+  const content = useCallback(() => {
+    switch (authStatus) {
+      case AuthStatus.Loading:
+        return (
+          <AuthStatusMessage key="Loading Message" status={authStatus}>
+            Loading...
+          </AuthStatusMessage>
+        );
+      case AuthStatus.Error:
+        return (
+          <AuthStatusMessage key="Error Message" status={authStatus}>
+            Failed to send link : (
+          </AuthStatusMessage>
+        );
+      case AuthStatus.Success:
+        return (
+          <AuthStatusMessage key="Success Message" status={authStatus}>
+            Link sent successfully :)
+          </AuthStatusMessage>
+        );
+      case AuthStatus.IDLE:
+      default:
+        return (
+          <FlatButton isPrimary disabled={!emailIsValid}>
+            Send link <FaArrowRight />
+          </FlatButton>
+        );
+    }
+  }, [authStatus, emailIsValid])();
+
+  const submitHandler = async (event: any) => {
     event.preventDefault();
 
     if (!emailIsValid) {
       return;
+    }
+
+    const result = await dispatch(sendLinkToResetPass(emailValue));
+
+    dispatch(updateAuthStatusAfterTime(AuthStatus.IDLE));
+
+    if (result.meta.requestStatus === "fulfilled") {
+      setTimeout(() => {
+        resetEmail();
+        history.push("/login");
+      }, 2000);
     }
 
     console.log("Submitted!");
@@ -50,11 +106,7 @@ const ResetPassCard: React.FC = () => {
             Please enter a valid email address.
           </ErrotInputTextStyled>
         )}
-        <AnimatedDivStyled>
-          <FlatButton isPrimary>
-            Send link <FaArrowRight />
-          </FlatButton>
-        </AnimatedDivStyled>
+        <AnimatedDivStyled>{content}</AnimatedDivStyled>
       </StyledForm>
     </Card>
   );

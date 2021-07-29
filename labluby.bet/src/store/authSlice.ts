bf22886cd1388b91a1e4d1d53a7357b0c565c372
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-29 13:23:16 -0400

    feat: Thunk function implmentation and AuthStatus dynamic change

--- labluby.bet/src/store/authSlice.ts
+++ labluby.bet/src/store/authSlice.ts
@@ -1,14 +1,25 @@
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from ".";
 import User from "../models/User";
-import { loginInAPI } from "../datasource/authDataSource";
+import {
+  loginInAPI,
+  registerUserInAPI,
+  resetPasswordInAPI,
+} from "../datasource/authDataSource";
 
+export enum AuthStatus {
+  Loading,
+  Logged,
+  Error,
+  IDLE,
+}
 export interface AuthState {
   userId: string | null;
   userToken: string | null;
   userName: string | null;
   userEmail: string | null;
 
+  status: AuthStatus;
   isLoggedIn: boolean;
   isLoading: boolean;
 }
@@ -19,6 +30,7 @@ const initialState: AuthState = {
   userName: null,
   userEmail: null,
 
+  status: AuthStatus.IDLE,
   isLoggedIn: false,
   isLoading: false,
 };
@@ -28,14 +40,55 @@ export interface LoginProps {
   password: string;
 }
 
+export interface RegisterProps {
+  name: string;
+  email: string;
+  password: string;
+}
+
 export const login = createAsyncThunk(
   "auth/login",
-  async (props: LoginProps) => {
+  async (props: LoginProps, thunkApi) => {
     const response = await loginInAPI(props.email, props.password);
+
+    if (!response) {
+      return thunkApi.rejectWithValue("Não foi possível fazer o Login !");
+    }
+
+    return { ...response.data };
+  }
+);
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (props: RegisterProps) => {
+    const response = await registerUserInAPI(
+      props.name,
+      props.email,
+      props.password
+    );
     // The value we return becomes the `fulfilled` action payload
     return { ...response.data };
   }
 );
+
+export const sendLinkToResetPass = createAsyncThunk(
+  "auth/sendLinkToResetPass",
+  async (email: string) => {
+    const response = await resetPasswordInAPI(email);
+
+    return { ...response.data };
+  }
+);
+
+export const updateAuthStatusAfterTime = createAsyncThunk<
+  AuthStatus,
+  AuthStatus
+>("auth/updateStatus", async (newStatus: AuthStatus = AuthStatus.IDLE) => {
+  return new Promise<AuthStatus>((resolve) => {
+    setTimeout(() => resolve(newStatus), 3000);
+  }).then((value) => value);
+});
 
 export const authSlice = createSlice({
   name: "auth",
@@ -48,6 +101,11 @@ export const authSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(login.pending, (state, action) => {
       state.isLoading = true;
+      state.status = AuthStatus.Loading;
+    });
+
+    builder.addCase(login.rejected, (state, action) => {
+      state.status = AuthStatus.Error;
     });
 
     builder.addCase(login.fulfilled, (state, action) => {
@@ -60,14 +118,22 @@ export const authSlice = createSlice({
         state.userName = user.name;
         state.userEmail = user.email;
 
+        state.status = AuthStatus.Logged;
         state.isLoggedIn = true;
       }
+    });
+
+    builder.addCase(updateAuthStatusAfterTime.fulfilled, (state, action) => {
+      console.log(action.payload);
+      state.status = action.payload;
     });
   },
 });
 
 export const selectIsLoggedInValue = (state: RootState) =>
-  state.auth.isLoggedIn;
+  state.auth.status === AuthStatus.Logged;
+
+export const selectAuthStatusValue = (state: RootState) => state.auth.status;
 
 export const selectUserData = (state: RootState) => {
   return {

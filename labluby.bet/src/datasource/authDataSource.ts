3dc3c86ff60588dac86bd112f1c384ba2d2a71ee
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-29 13:20:37 -0400

    feat: Define register and reset API function

--- labluby.bet/src/datasource/authDataSource.ts
+++ labluby.bet/src/datasource/authDataSource.ts
@@ -11,3 +11,31 @@ export const loginInAPI = async (email: string, password: string) => {
     )
   );
 };
+
+export const registerUserInAPI = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  return new Promise<{ data: User }>((resolve) =>
+    setTimeout(
+      () =>
+        resolve({
+          data: new User(Math.random().toString(), name, email, password),
+        }),
+      500
+    )
+  );
+};
+
+export const resetPasswordInAPI = async (email: string) => {
+  return new Promise<{ data: User }>((resolve) =>
+    setTimeout(
+      () =>
+        resolve({
+          data: new User(Math.random().toString(), "Teste", email, 'password'),
+        }),
+      500
+    )
+  );
+};

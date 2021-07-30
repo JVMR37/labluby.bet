9d0e54b665e2b50a24cf2dc4a2ca134c5fda9349
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-30 11:57:23 -0400

    refactor: Rename CartItem model to SavedGame

---
+++ labluby.bet/src/models/SavedGame.ts
@@ -0,0 +1,24 @@
+import Game from "./Game";
+
+export default class SavedGame {
+  id: number;
+  selectedNumbers: Array<string>;
+  typeGame: Game;
+  createdAt: Date;
+  price: number;
+
+  constructor(selectedNumbers: Array<string>, typeGame: Game, price: number) {
+    this.id = Date.now();
+    this.createdAt = new Date();
+    this.selectedNumbers = selectedNumbers;
+    this.typeGame = typeGame;
+    this.price = price;
+  }
+
+  getCreatedAt(): String {
+    const dd = String(this.createdAt.getUTCDate()).padStart(2, "0");
+    const mm = String(this.createdAt.getUTCMonth() + 1).padStart(2, "0"); //January is 0!
+    const yyyy = this.createdAt.getUTCFullYear();
+    return mm + '/' + dd + '/' + yyyy;
+  }
+}

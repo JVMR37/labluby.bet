ca411aae5352f2adeac9e44d6146361cbacf4bd3
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-30 11:58:51 -0400

    feat: Init NewBet page implementation

--- labluby.bet/src/pages/NewBet.tsx
+++ labluby.bet/src/pages/NewBet.tsx
@@ -1,6 +1,18 @@
+import { CenteredPageDiv, Column, Row } from "../GlobalStyles";
+import CartCard from "../components/CartCard";
+
 const NewBet: React.FC = () => {
   //TODO: Implement NewBet Page
-    return <div></div>;
+  return (
+    <CenteredPageDiv>
+      <Row>
+        <Column width="70%" margin="0px 5rem"></Column>
+        <Column width="30%" margin="0px 5rem">
+          <CartCard />
+        </Column>
+      </Row>
+    </CenteredPageDiv>
+  );
 };
 
 export default NewBet;

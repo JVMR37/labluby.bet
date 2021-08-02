a59c364e471efdd493f73a56638570bdfd43c8bb
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-02 14:18:35 -0400

    feat: init home page implementation

--- labluby.bet/src/pages/Home.tsx
+++ labluby.bet/src/pages/Home.tsx
@@ -1,7 +1,55 @@
-const Home: React.FC = () => {
-  //TODO: Implement Home Page
+import { useHistory } from "react-router-dom";
+import {
+  HomeRow,
+  RecentGameStyledSpan,
+  FilterStyledSpan,
+  FilterStyledDiv,
+  NewBetStyledDiv,
+  FilterGameStyledButton,
+  NewBetStyledButton,
+} from "../styles/home.style";
 
-  return <div></div>;
+import { useAppDispatch } from "../hooks/hooks";
+import { loadGames } from "../store/gamesSlice";
+import { FaArrowRight } from "react-icons/fa";
+
+const Home: React.FC = () => {
+  const history = useHistory();
+  const dispatch = useAppDispatch();
+
+  const newBetButtonHandler = async (event: any) => {
+    event.preventDefault();
+
+    const result = await dispatch(loadGames());
+
+    if (result.meta.requestStatus === "fulfilled") {
+      history.push("/new-bet");
+    }
+  };
+
+  return (
+    <HomeRow>
+      <RecentGameStyledSpan>RECENT GAME</RecentGameStyledSpan>
+      <FilterStyledDiv>
+        <FilterStyledSpan>Filters</FilterStyledSpan>
+        <FilterGameStyledButton gameColor={"red"}>
+          Lotofácil
+        </FilterGameStyledButton>
+
+        <FilterGameStyledButton gameColor={"green"}>
+          Lotomania
+        </FilterGameStyledButton>
+        <FilterGameStyledButton gameColor={"tea"}>
+          MegaSena
+        </FilterGameStyledButton>
+      </FilterStyledDiv>
+      <NewBetStyledDiv>
+        <NewBetStyledButton isPrimary onClick={newBetButtonHandler}>
+          New Bet <FaArrowRight />
+        </NewBetStyledButton>
+      </NewBetStyledDiv>
+    </HomeRow>
+  );
 };
 
 export default Home;
---
+++ labluby.bet/src/styles/home.style.ts
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { OutlineButton, FlatButton } from "../GlobalStyles";
+
+export const HomeRow = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: flex-start;
+  width: 100%;
+`;
+
+export const RecentGameStyledSpan = styled.span`
+  display: flex;
+  flex: 2;
+  justify-self: flex-start;
+  font: italic normal bold 3rem Helvetica;
+  color: ${({ theme }) => theme.colors.secondary};
+  text-transform: uppercase;
+`;
+
+export const FilterStyledSpan = styled.span`
+  display: flex;
+  font: italic normal normal 17px/70px Helvetica;
+  letter-spacing: 0px;
+  color: #868686;
+`;
+
+export const FilterStyledDiv = styled.div`
+  display: flex;
+  flex: 4;
+  justify-self: flex-start;
+  margin: 2rem;
+
+  justify-content: space-around;
+  align-content: space-around;
+  align-items: center;
+`;
+
+export const NewBetStyledDiv = styled.div`
+  display: flex;
+  flex: 2;
+  justify-self: flex-end;
+  justify-content: flex-end;
+  align-items: flex-start;
+
+  @media only screen and (max-width: 1000px) {
+    justify-content: flex-start !important;
+  }
+`;
+
+export const NewBetStyledButton = styled(FlatButton)`
+  width: inherit;
+`;
+
+export const FilterGameStyledButton = styled(OutlineButton)<{
+  gameColor: string;
+  isSelected?: boolean;
+}>`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+
+  padding: 1rem;
+  margin: 1rem;
+
+  border: 2px solid ${({ gameColor }) => gameColor};
+  border-radius: 50rem;
+
+  color: ${({ gameColor, isSelected }) => (isSelected ? "white" : gameColor)};
+  background-color: ${({ gameColor, isSelected, theme }) =>
+    isSelected ? gameColor : theme.colors.background};
+  font-weight: ${({ isSelected }) => (isSelected ? "bold" : "normal")};
+
+  pointer-events: ${({ isSelected }) => (isSelected ? "none" : "all")};
+
+  &:hover {
+    background-color: ${({ gameColor }) => gameColor};
+    color: white;
+  }
+`;

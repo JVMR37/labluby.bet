27877497269c13d5d9aa06ac61ff8f1474b56fd2
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-02 19:37:00 -0400

    feat: implementation of game and action buttons

--- labluby.bet/src/components/NewBetComponent.tsx
+++ labluby.bet/src/components/NewBetComponent.tsx
@@ -1,25 +1,81 @@
-import { Row, Column } from "../GlobalStyles";
+import { useCallback } from "react";
+import { FaShoppingCart } from "react-icons/fa";
 import {
   NewBetStyledTitle,
   GameStyledButton,
+  NewBetContentStyledColumn,
+  ChooseGameStyledSpan,
+  TypeGameButtonStyledRow,
+  DescriptionGameStyledSpan,
+  DescriptionTitleStyledSpan,
+  AddToCartStyledButton,
+  GameActionStyledButton,
+  GameActionsStyledRow,
+  CartButtonStyledDiv,
 } from "../styles/newBetComponent.style";
+
+import { useAppSelector, useAppDispatch } from "../hooks/hooks";
+import {
+  selectAvailableGames,
+  getSelectedGame,
+  selectGame,
+} from "../store/gamesSlice";
+
 const NewBetComponent: React.FC = (props) => {
+  const dispatch = useAppDispatch();
+  const availableGames = useAppSelector(selectAvailableGames);
+  const selectedGame = useAppSelector(getSelectedGame);
+
+  const gameButtonHandler = useCallback(
+    (event: any) => {
+      console.log("========== Game Button Clicked ==========");
+      const gameName = event.target.textContent;
+      dispatch(selectGame(gameName));
+    },
+    [dispatch]
+  );
+
+  const gameButtons = useCallback(() => {
+    return availableGames.map((game) => {
+      return (
+        <GameStyledButton
+          key={game.type}
+          isSelected={game === selectedGame}
+          gameColor={game.color}
+          onClick={gameButtonHandler}
+        >
+          {game.type}
+        </GameStyledButton>
+      );
+    });
+  }, [availableGames, selectedGame, gameButtonHandler])();
+
   return (
-    <Column>
+    <NewBetContentStyledColumn>
       <NewBetStyledTitle>
         New Bet
-        <span>{"for game tal"}</span>
+        <span>{" for game Lotofácil"}</span>
       </NewBetStyledTitle>
 
-      <span>Choose a game</span>
+      <ChooseGameStyledSpan>Choose a game</ChooseGameStyledSpan>
 
-      <Row>
-        <GameStyledButton gameColor={"red"}>Jogo</GameStyledButton>
-      </Row>
+      <TypeGameButtonStyledRow>{gameButtons} </TypeGameButtonStyledRow>
 
-      <span>Fill your bet</span>
-      <span>Description</span>
-    </Column>
+      <DescriptionTitleStyledSpan>Fill your bet</DescriptionTitleStyledSpan>
+      <DescriptionGameStyledSpan>
+        {selectedGame?.description}
+      </DescriptionGameStyledSpan>
+
+      <GameActionsStyledRow>
+        <GameActionStyledButton isPrimary>Complete Game</GameActionStyledButton>
+        <GameActionStyledButton isPrimary>Clear Game</GameActionStyledButton>
+        <CartButtonStyledDiv>
+          <AddToCartStyledButton isPrimary>
+            <FaShoppingCart /> Add to cart
+          </AddToCartStyledButton>
+        </CartButtonStyledDiv>
+      </GameActionsStyledRow>
+    </NewBetContentStyledColumn>
   );
 };
 
--- labluby.bet/src/styles/newBetComponent.style.ts
+++ labluby.bet/src/styles/newBetComponent.style.ts
@@ -1,21 +1,49 @@
 import styled from "styled-components";
-import { OutlineButton } from "../GlobalStyles";
+import { OutlineButton, Button } from "../GlobalStyles";
+
+export const NewBetContentStyledColumn = styled.div`
+  display: flex;
+  flex: 6 6;
+  flex-flow: column wrap;
+  padding-right: 3rem;
+`;
 
 export const NewBetStyledTitle = styled.div`
   text-align: start;
-  font: italic normal bold 24px Helvetica;
+  font: italic normal bold 3rem Helvetica;
   letter-spacing: 0px;
   color: ${({ theme }) => theme.colors.secondary};
   text-transform: uppercase;
-  margin: 0;
+  margin-top: 2.7rem;
+  margin-bottom: 3rem;
   padding: 0;
 
   span:nth-child(1) {
     text-align: center;
-    font: italic normal 300 24px Helvetica;
+    font: italic normal 300 3rem Helvetica;
     letter-spacing: 0px;
-    color: red;
     text-transform: uppercase;
+  }
+`;
+
+export const ChooseGameStyledSpan = styled.span`
+  text-align: start;
+  font: italic normal bold 2rem Helvetica;
+  letter-spacing: 0px;
+  color: ${({ theme }) => theme.colors.secondary};
+  margin-bottom: 2rem;
+`;
+
+export const TypeGameButtonStyledRow = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: flex-start;
+  margin-bottom: 2rem;
+
+  @media only screen and (max-width: 1000px) {
+    justify-content: start;
   }
 `;
 
@@ -23,6 +51,10 @@ export const GameStyledButton = styled(OutlineButton)<{
   gameColor: string;
   isSelected?: boolean;
 }>`
+  width: 13rem;
+  padding: 1rem;
+  margin: 1rem 1rem;
+
   border: 2px solid ${({ gameColor }) => gameColor};
   border-radius: 50rem;
 
@@ -37,4 +69,75 @@ export const GameStyledButton = styled(OutlineButton)<{
     background-color: ${({ gameColor }) => gameColor};
     color: white;
   }
+
+  @media only screen and (max-width: 1000px) {
+    /* width: 10rem; */
+  }
+`;
+
+export const DescriptionTitleStyledSpan = styled.span`
+  text-align: left;
+  font: italic normal bold 2rem Helvetica;
+  letter-spacing: 0px;
+  color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export const DescriptionGameStyledSpan = styled.span`
+  text-align: left;
+  letter-spacing: 0px;
+  color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export const GameActionsStyledRow = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: flex-start;
+  padding: 1px;
+  margin: 2rem 0rem;
+
+  @media only screen and (max-width: 1000px) {
+    justify-content: flex-start !important;
+    flex: inherit;
+  }
+`;
+
+export const GameActionStyledButton = styled(OutlineButton)`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50rem;
+  padding: 1rem 2rem;
+  margin: 1rem 1rem;
+
+  @media only screen and (max-width: 1000px) {
+    flex: inherit;
+  }
+`;
+
+export const CartButtonStyledDiv = styled.div`
+  display: flex;
+  flex: 2;
+  padding: 5px;
+  justify-self: flex-end;
+  justify-content: flex-end;
+  align-items: flex-start;
+
+  @media only screen and (max-width: 1000px) {
+    justify-content: flex-start !important;
+    flex: inherit;
+  }
+`;
+
+export const AddToCartStyledButton = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+
+  /* width: 13rem; */
+  padding: 1rem 3rem;
+  margin: 1rem 1rem;
 `;

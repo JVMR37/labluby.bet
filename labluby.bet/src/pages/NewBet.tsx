60fa7985b538768cef9e7d3a15b3c3cd14ad1b5e
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-02 19:37:45 -0400

    refactor: using specific components for column and rows on New Bet Page

--- labluby.bet/src/pages/NewBet.tsx
+++ labluby.bet/src/pages/NewBet.tsx
@@ -1,20 +1,15 @@
-import { CenteredPageDiv, Column, Row } from "../GlobalStyles";
 import CartCard from "../components/CartCard";
 import NewBetComponent from "../components/NewBetComponent";
+import { NewBetStyledRow, CartCardStyledColumn } from "../styles/newBet.style";
 
 const NewBet: React.FC = () => {
-  //TODO: Implement NewBet Page
   return (
-    <CenteredPageDiv>
-      <Row>
-        <Column width="70%" margin="0px 5rem">
-          <NewBetComponent />
-        </Column>
-        <Column width="30%" margin="0px 5rem">
-          <CartCard />
-        </Column>
-      </Row>
-    </CenteredPageDiv>
+    <NewBetStyledRow>
+      <NewBetComponent />
+      <CartCardStyledColumn>
+        <CartCard />
+      </CartCardStyledColumn>
+    </NewBetStyledRow>
   );
 };
 
---
+++ labluby.bet/src/styles/newBet.style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const NewBetStyledRow = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const CartCardStyledColumn = styled.div`
+  display: flex;
+  flex: 4 4 fit;
+
+  flex-flow: column wrap;
+  margin-bottom: 5rem;
+`;

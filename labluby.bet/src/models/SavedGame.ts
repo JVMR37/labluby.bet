ae7b0a9a1014c6eac54d2182552bbb3ad0ee7472
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-03 12:07:19 -0400

    refactor: convert Array<String> to Array<Number>

--- labluby.bet/src/models/SavedGame.ts
+++ labluby.bet/src/models/SavedGame.ts
@@ -2,12 +2,12 @@ import Game from "./Game";
 
 export default class SavedGame {
   id: number;
-  selectedNumbers: Array<string>;
+  selectedNumbers: Array<number>;
   typeGame: Game;
   createdAt: Date;
   price: number;
 
-  constructor(selectedNumbers: Array<string>, typeGame: Game, price: number) {
+  constructor(selectedNumbers: Array<number>, typeGame: Game, price: number) {
     this.id = Date.now();
     this.createdAt = new Date();
     this.selectedNumbers = selectedNumbers;
@@ -19,6 +19,6 @@ export default class SavedGame {
     const dd = String(this.createdAt.getUTCDate()).padStart(2, "0");
     const mm = String(this.createdAt.getUTCMonth() + 1).padStart(2, "0"); //January is 0!
     const yyyy = this.createdAt.getUTCFullYear();
-    return mm + '/' + dd + '/' + yyyy;
+    return mm + "/" + dd + "/" + yyyy;
   }
 }

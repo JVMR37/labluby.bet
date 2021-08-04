deb439f50e8d6131fe691ddd6d554a3424cee6d3
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-04 12:29:23 -0400

    feat: ajusts and NoGames messasge's elements

--- labluby.bet/src/styles/recentGameComponent.style.ts
+++ labluby.bet/src/styles/recentGameComponent.style.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import { highlightAnimation } from "./animatedDiv.style";
 
 export const RecentGameStyledColumn = styled.div`
+  ${highlightAnimation}
   display: flex;
   flex-flow: column wrap;
   justify-content: space-around;
@@ -10,10 +12,9 @@ export const RecentGameStyledColumn = styled.div`
 export const GameStyledDiv = styled.div<{
   borderColor: string;
 }>`
-  flex: 3;
   display: flex;
-  min-width: 0;
-  min-height: 0;
+  flex-direction: column;
+  max-width: 70%;
   flex-wrap: wrap;
   word-break: break-all;
   word-wrap: break-word;
@@ -21,6 +22,7 @@ export const GameStyledDiv = styled.div<{
   border-bottom-left-radius: 0.25rem;
   border-top-left-radius: 0.25rem;
   border-left: 4px solid ${({ borderColor }) => borderColor};
+  margin: 1rem 2rem;
 `;
 
 export const NumbersStyledSpan = styled.span`
@@ -30,6 +32,12 @@ export const NumbersStyledSpan = styled.span`
   white-space: wrap;
   font: italic normal bold 1.9rem Helvetica;
   color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export const GameDatePriceRow = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  width: 100%;
 `;
 
 export const NameGameStyledSpan = styled.span<{
@@ -44,12 +52,47 @@ export const NameGameStyledSpan = styled.span<{
   text-transform: uppercase;
 `;
 
-export const PriceStyledSpan = styled.span`
+export const DatePriceStyledSpan = styled.span`
   display: flex;
 
   text-align: center;
   padding: 0.4rem 0.6rem 0rem 0;
+  margin: 0.7rem 0rem;
 
   font: normal normal normal 1.9rem Helvetica;
   color: #868686;
+`;
+
+export const NoGamesStyledDiv = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: space-evenly;
+  align-items: center;
+
+  margin: 5rem 0rem;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const NoGamesStyledSpan = styled.span`
+  text-align: center;
+  width: 100%;
+
+  font: normal normal 300 2rem Helvetica;
+  letter-spacing: 0px;
+  color: ${({ theme }) => theme.colors.secondary};
+  text-transform: uppercase;
+`;
+
+export const NoGamesBoldStyledSpan = styled.span`
+  text-align: center;
+  width: 100%;
+
+  margin: 1rem 0rem;
+
+  font: normal normal bold 2rem Helvetica;
+  letter-spacing: 0px;
+  color: ${({ theme }) => theme.colors.secondary};
+  text-transform: uppercase;
 `;

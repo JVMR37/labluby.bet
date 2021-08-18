5f48c99e282526ee98499655054a2ed68967355e
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-18 13:56:40 -0400

    feat: Using API for Autentication

--- labluby.bet/src/datasource/authDataSource.ts
+++ labluby.bet/src/datasource/authDataSource.ts
@@ -1,15 +1,24 @@
 import User from "../models/User";
+import ApiDatasource from "./apiDatasource";
 
 export const loginInAPI = async (email: string, password: string) => {
-  return new Promise<{ data: User }>((resolve) =>
-    setTimeout(
-      () =>
-        resolve({
-          data: new User(Math.random().toString(), "Teste", email, password),
-        }),
-      500
-    )
-  );
+  return new Promise<{ data: User }>(async (resolve) => {
+    var apiUtils = ApiDatasource.Instance;
+    const response = await apiUtils.Axios.post("/sessions", {
+      email: email,
+      password: password,
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response);
+      }
+    });
+    console.log(response);
+    resolve({
+      data: new User(Math.random().toString(), "Teste", email, password),
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
 };
 
 export const registerUserInAPI = async (
@@ -33,7 +42,7 @@ export const resetPasswordInAPI = async (email: string) => {
     setTimeout(
       () =>
         resolve({
-          data: new User(Math.random().toString(), "Teste", email, 'password'),
+          data: new User(Math.random().toString(), "Teste", email, "password"),
         }),
       500
     )

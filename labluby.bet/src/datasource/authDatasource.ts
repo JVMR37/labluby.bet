76269a72e4ce271131b6bb421b277131ee15b077
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-20 19:08:44 -0400

    feat: Conenctando funções com o back

--- labluby.bet/src/datasource/authDatasource.ts
+++ labluby.bet/src/datasource/authDatasource.ts
@@ -31,25 +31,81 @@ export const registerUserInAPI = async (
   email: string,
   password: string
 ) => {
-  return new Promise<{ data: User }>((resolve) =>
-    setTimeout(
-      () =>
+  return new Promise<{ data: User } | Error>(async (resolve) => {
+    var apiUtils = ApiDatasource.Instance;
+    apiUtils.Axios.post("/users", {
+      name: name,
+      email: email,
+      password: password,
+    })
+      .then((response) => {
+        console.log(response);
+
         resolve({
-          data: new User(Math.random().toString(), name, email, password),
-        }),
-      500
-    )
-  );
+          data: User.fromJSON(response!.data),
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          resolve(Error(error.response.error));
+        }
+      });
+  });
 };
 
-export const resetPasswordInAPI = async (email: string) => {
-  return new Promise<{ data: User }>((resolve) =>
-    setTimeout(
-      () =>
+export const requestPasswordChange = async (email: string) => {
+  return new Promise<{ data: User } | Error>(async (resolve) => {
+    var apiUtils = ApiDatasource.Instance;
+    apiUtils.Axios.post("/passwords", {
+      email: email,
+      redirect_url: "http://localhost:3000/update-password",
+    })
+      .then((response) => {
+        console.log(response);
+
+        apiUtils.setToken(response.data.token);
+
         resolve({
-          data: new User(Math.random().toString(), "Teste", email, "password"),
-        }),
-      500
-    )
-  );
+          data: User.fromJSON(response!.data),
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          resolve(Error(error.response.error));
+        }
+      });
+  });
+};
+
+export const sendNewPassword = async (
+  token: string,
+  password: string,
+  password_confirmation: string
+) => {
+  return new Promise<{ data: User } | Error>(async (resolve) => {
+    var apiUtils = ApiDatasource.Instance;
+
+    apiUtils.Axios.put("/passwords", {
+      token,
+      password,
+      password_confirmation,
+    })
+      .then((response) => {
+        console.log(response);
+
+        apiUtils.setToken(response.data.token);
+
+        resolve({
+          data: User.fromJSON(response!.data),
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          resolve(Error(error.response.error));
+        }
+      });
+  });
 };

476c5b273cd6cace7f34e417425d638dc5d4d1e4
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-20 15:05:29 -0400

    feat: Add userToken como atributo

--- labluby.bet/src/models/User.ts
+++ labluby.bet/src/models/User.ts
@@ -3,15 +3,23 @@ export default class User {
   name: string;
   email: string;
   password: string;
+  token?: string;
 
-  constructor(id: string, name: string, email: string, password: string) {
+  constructor(id: string, name: string, email: string, password: string, token?: string) {
     this.id = id;
     this.name = name;
     this.email = email;
     this.password = password;
+    this.token = token;
   }
 
-  public static fromJSON(json: { id: string; name: string; email: string; password: string; }): User {
-    return new User(json.id, json.name, json.email, json.password);
+  public static fromJSON(json: {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    token?: string;
+  }): User {
+    return new User(json.id, json.name, json.email, json.password, json.token);
   }
 }

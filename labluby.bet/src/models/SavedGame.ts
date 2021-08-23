68901e43cae0edaadbacd7235cc8ac3048e4c351
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-23 18:59:10 -0400

    refactor: Add wrappers e ajustes na classe SavedGame

--- labluby.bet/src/models/SavedGame.ts
+++ labluby.bet/src/models/SavedGame.ts
@@ -4,21 +4,53 @@ export default class SavedGame {
   id: number;
   numbers: Array<number>;
   betType: Game;
-  createdAt: Date;
+  createdAt?: Date;
   price: number;
 
-  constructor(selectedNumbers: Array<number>, typeGame: Game, price: number) {
-    this.id = Date.now();
-    this.createdAt = new Date();
+  constructor(
+    selectedNumbers: Array<number>,
+    typeGame: Game,
+    price: number,
+    createdAt?: Date | String,
+    id?: number
+  ) {
+    this.id = id ? id : Date.now();
+
     this.numbers = selectedNumbers;
     this.betType = typeGame;
     this.price = price;
+
+    if (createdAt) {
+      this.createdAt = new Date(createdAt as string);
+    }
+  }
+
+  public static fromJson(json: any): SavedGame {
+    return new SavedGame(
+      json.numbers,
+      json.betType,
+      json.price,
+      new Date(json.createdAt),
+      json.id
+    );
+  }
+
+  public toJson(): any {
+    return {
+      numbers: this.numbers,
+      price: this.price,
+      typeId: this.betType.id,
+    };
   }
 
   getCreatedAt(): String {
-    const dd = String(this.createdAt.getUTCDate()).padStart(2, "0");
-    const mm = String(this.createdAt.getUTCMonth() + 1).padStart(2, "0"); //January is 0!
-    const yyyy = this.createdAt.getUTCFullYear();
-    return mm + "/" + dd + "/" + yyyy;
+    if (this.createdAt) {
+      const dd = String(this.createdAt.getUTCDate()).padStart(2, "0");
+      const mm = String(this.createdAt.getUTCMonth() + 1).padStart(2, "0"); //January is 0!
+      const yyyy = this.createdAt.getUTCFullYear();
+      return mm + "/" + dd + "/" + yyyy;
+    } else {
+      return "No Date To Show.";
+    }
   }
 }

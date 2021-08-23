80d5ec859c3e23208e20ea9b723f7328a7520941
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-23 18:58:12 -0400

    fix: Tipando adequadamente o retorno

--- labluby.bet/src/datasource/gameDatasource.ts
+++ labluby.bet/src/datasource/gameDatasource.ts
@@ -2,6 +2,7 @@ import { AxiosResponse } from "axios";
 import Game from "../models/Game";
 import SavedGame from "../models/SavedGame";
 import ApiDatasource from "./apiDatasource";
+import PaginationMetadata from "../models/PaginationMetadata";
 
 export const fetchGames = async () => {
   return new Promise<{ data: Game[] }>((resolve, reject) => {
@@ -20,19 +21,28 @@ export const fetchGames = async () => {
 };
 
 export const fetchSavedBets = async (page: number = 1, filter?: string) => {
-  return new Promise<{ data: SavedGame[] }>((resolve, reject) => {
-    ApiDatasource.Instance.Axios.get(
-      `/bets?page=${page}${filter ? "&filter=" + filter : ""}`
-    )
-      .then((response) => {
-        console.log(response.data);
-        resolve({ data: response.data.data });
-      })
-      .catch((error) => {
-        console.log(error);
-        reject(error);
-      });
-  }).catch((error) => {
+  return new Promise<{ data: SavedGame[]; meta: PaginationMetadata }>(
+    (resolve, reject) => {
+      ApiDatasource.Instance.Axios.get(
+        `/bets?page=${page}${filter ? "&filter=" + filter : ""}`
+      )
+        .then((response) => {
+          console.log(response.data);
+          const responseData = response.data.data.map((betsJson: any) =>
+            SavedGame.fromJson(betsJson)
+          );
+          resolve({ data: responseData, meta: response.data.meta });
+        })
+        .catch((error) => {
+          if (error.messages) {
+            console.log(error.messages);
+          } else {
+            console.log(error);
+          }
+          reject(error);
+        });
+    }
+  ).catch((error) => {
     return Error(error.message);
   });
 };
@@ -42,7 +52,7 @@ export const saveBetsInDB = async (
 ): Promise<AxiosResponse | Error> => {
   return new Promise<AxiosResponse | Error>((resolve, reject) => {
     ApiDatasource.Instance.Axios.post("/bets", {
-      bets: bets,
+      bets: bets.map((bet) => bet.toJson()),
     })
       .then((response) => {
         console.log(response.data);
